fa81d56c10820ed743f0e204981328161af064f8
Jerome <21400749@handong.edu>
2021-10-05 23:49:37 +0900

    Add createDiary method and done Postman test

--- src/diaries/diaries.service.ts
+++ src/diaries/diaries.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Diary } from './diary.model';
+import { Diary, DiaryStatus } from './diary.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class DiariesService {
@@ -7,5 +8,17 @@ export class DiariesService {
 
   getAllDiaries() {
     return this.diaries;
+  }
+
+  createDiary(title: string, date: Date, content: string): Diary {
+    const diary: Diary = {
+      id: uuid(),
+      title: title,
+      date: date,
+      content: content,
+      status: DiaryStatus.NEW,
+    };
+    this.diaries.push(diary); // Diary에 푸시를 해서 새 일기를 기록함
+    return diary;
   }
 }
