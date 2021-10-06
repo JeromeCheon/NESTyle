35f759a394d3aeeece5fef1fab02be0f198c1a7c
Jerome <cheonjh4572@gmail.com>
2021-10-06 23:23:23 +0900

    Add getDiariesByMonth method and tested at Postman

--- src/diaries/diaries.controller.ts
+++ src/diaries/diaries.controller.ts
@@ -1,22 +1,26 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { DiariesService } from './diaries.service';
 import { Diary } from './diary.model';
 
 @Controller('diaries')
 export class DiariesController {
-  constructor(private diariesService: DiariesService) { }
+  constructor(private diariesService: DiariesService) {}
 
   @Get()
   getAllDiaries(): Diary[] {
     return this.diariesService.getAllDiaries();
   }
 
+  @Get('/:month')
+  getDiaryByMonth(@Param('month') month: string): Diary[] {
+    return this.diariesService.getDiariesByMonth(month);
+  }
+
   @Post()
   createDiary(
     @Body('title') title: string,
-    @Body('date') date: Date,
     @Body('content') content: string,
   ): Diary {
-    return this.diariesService.createDiary(title, date, content);
+    return this.diariesService.createDiary(title, content);
   }
 }
