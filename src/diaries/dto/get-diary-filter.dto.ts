5a4335e2e5f0d82454ab34650fed37a78148c9da
Jerome <cheonjh4572@gmail.com>
2021-10-20 23:22:39 +0900

    Add validation pipe in main file and IsNotEmpty() in diary DTO

--- src/diaries/dto/get-diary-filter.dto.ts
+++ src/diaries/dto/get-diary-filter.dto.ts
@@ -1,6 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
 import { DiaryStatus } from '../diary.model';
 
 export class GetDiariesFilterDto {
+  @IsNotEmpty()
   status: DiaryStatus;
+  @IsNotEmpty()
   search: string;
 }
