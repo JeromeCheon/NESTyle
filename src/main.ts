9b612385416f111b3d33cad82019e961342c1c6d
Jerome <21400749@handong.edu>
2021-10-20 23:22:39 +0900

    Add validation pipe in main file and IsNotEmpty() in diary DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // validation pipe를 추가해줌으로써 empty value를 검사함
   await app.listen(3000);
 }
 bootstrap();
