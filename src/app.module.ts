95ebb9f58554bb2fc4683e663dc9de889d21b5ab
Jerome <21400749@handong.edu>
2021-10-24 00:23:22 +0900

    Add TypeOrmModule setting in app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { DiariesController } from './diaries/diaries.controller';
 import { DiariesService } from './diaries/diaries.service';
 import { DiariesModule } from './diaries/diaries.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DiariesModule],
+  imports: [
+    DiariesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'Nestyle',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
