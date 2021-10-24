868aa7022cd90afe5222d0b51a44b0b98b4cadbb
Jerome <cheonjh4572@gmail.com>
2021-10-24 13:49:36 +0900

    Update getDiaryById method with typeorm findONe method

--- src/diaries/diaries.controller.ts
+++ src/diaries/diaries.controller.ts
@@ -18,43 +18,43 @@ import { UpdateDiaryStatusDto } from './dto/update-diary-status.dto';
 export class DiariesController {
   constructor(private diariesService: DiariesService) {}
 
-  @Get()
-  getDiaries(@Query() filterDto: GetDiariesFilterDto): Diary[] {
-    if (Object.keys(filterDto).length) {
-      return this.diariesService.getDiariesWithFilters(filterDto);
-    } else {
-      return this.diariesService.getAllDiaries();
-    }
-  }
+  // @Get()
+  // getDiaries(@Query() filterDto: GetDiariesFilterDto): Diary[] {
+  //   if (Object.keys(filterDto).length) {
+  //     return this.diariesService.getDiariesWithFilters(filterDto);
+  //   } else {
+  //     return this.diariesService.getAllDiaries();
+  //   }
+  // }
 
   @Get('/:id')
-  getDiaryById(@Param('id') id: string): Diary {
+  getDiaryById(@Param('id') id: string): Promise<Diary> {
     return this.diariesService.getDiaryById(id);
   }
 
-  @Get()
-  getDiariesByMonth(@Query() month: string): Diary[] {
-    return this.diariesService.getDiariesByMonth(month);
-  }
+  // @Get()
+  // getDiariesByMonth(@Query() month: string): Diary[] {
+  //   return this.diariesService.getDiariesByMonth(month);
+  // }
 
-  @Post()
-  createDiary(@Body() createDiaryDto: CreateDiaryDto): Diary {
-    return this.diariesService.createDiary(createDiaryDto);
-  }
+  // @Post()
+  // createDiary(@Body() createDiaryDto: CreateDiaryDto): Diary {
+  //   return this.diariesService.createDiary(createDiaryDto);
+  // }
 
-  @Delete('/:id')
-  deleteDiary(@Param('id') id: string): void {
-    return this.diariesService.deleteDiary(id);
-  }
+  // @Delete('/:id')
+  // deleteDiary(@Param('id') id: string): void {
+  //   return this.diariesService.deleteDiary(id);
+  // }
 
-  @Patch('/:id')
-  updateTitleNContent(
-    @Param('id') id: string,
-    @Body('title') title: string,
-    @Body('content') content: string,
-    @Body() updateDiaryStatusDto: UpdateDiaryStatusDto,
-  ): Diary {
-    const { status } = updateDiaryStatusDto;
-    return this.diariesService.updateTitleNContent(id, title, content, status);
-  }
+  // @Patch('/:id')
+  // updateTitleNContent(
+  //   @Param('id') id: string,
+  //   @Body('title') title: string,
+  //   @Body('content') content: string,
+  //   @Body() updateDiaryStatusDto: UpdateDiaryStatusDto,
+  // ): Diary {
+  //   const { status } = updateDiaryStatusDto;
+  //   return this.diariesService.updateTitleNContent(id, title, content, status);
+  // }
 }
